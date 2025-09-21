4c45fd68a3dcb6b2705604ee5db94cdff2a7bda0
Robel Asaminew <robelas2001@gmail.com>
2025-09-21 19:32:28 +0000

    disable the view result button on the auction status unless the bid ends

--- src/app/auctions/[id]/page.tsx
+++ src/app/auctions/[id]/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { getAuctionItemForListing } from "@/lib/data/public";
-import { notFound, useParams } from "next/navigation";
+import { useParams } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
